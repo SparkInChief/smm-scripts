30bd0fa39bbcd55e5b1bc9de90915b09c3e52050
SparkInChief <brian@egjarts.org>
2017-06-03 12:06:15 -0700

    Update submissiondetail.js

--- submissiondetail.js
+++ submissiondetail.js
@@ -22,7 +22,7 @@ function enhanceReviewPanel() {
     //     $("#prev-submission").clone().toggleClass("inline-block btn btn-primary pull-right").css("font-size", "15px").insertBefore("#save-draft-review").children("span").attr("style", "font-size:15px!important")
     // }
     if ($("div.label-content:contains(No Feedback)").length) {
-        $("#ReviewEntryForm label:contains(Submitter Notes)").append('<span>Optional</span>');
+        $("#ReviewEntryForm label:contains(Submitter Notes)").append('<span> (Optional)</span>');
     } else {
         $("#ReviewEntryForm label:contains(Submitter Notes)").append('<span class="required" aria-required="true">*</span>').nextAll("textarea").addClass("required").attr("aria-required", "true");
     }
